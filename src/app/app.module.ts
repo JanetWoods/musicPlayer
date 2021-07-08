0fe213396110c43983adcc4847d2e2d87960605e
Janet Woods <janetnwoods@gmail.com>
2021-07-08 11:31:30 -0400

    isolated nav section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import {appRoutes} from './routes';
 import {ArtistComponent} from './artists/artist';
-import { AlbumsComponent } from './albums/albums.component'
+import { AlbumsComponent } from './albums/albums.component';
+import { NavigationComponent } from './navigation/navigation.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ArtistComponent,
-    AlbumsComponent
+    AlbumsComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
