03ab20a6cb44442c7bf7874e62021263fbd25b47
Janet Woods <janetnwoods@gmail.com>
2021-07-12 11:15:48 -0400

    done

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
+  styleUrls: ['../app.component.scss']
 })
 export class PlayerComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
+  title = "player";
 
 }
